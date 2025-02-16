4d1d2218a73218b05aaa7beab4b163e04f4930ac
Jedrek00 <jedrek.koscielniak@gmail.com>
2025-02-16 18:52:23 +0100

    Add example with OpenAI model

---
+++ main.py
@@ -0,0 +1,22 @@
+from langchain_openai import OpenAI
+from langchain_core.prompts import PromptTemplate
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = OpenAI()
+response = llm.invoke("Hello how are you?")
+print(response)
+
+prompt = PromptTemplate.from_template("How to say {input} in {output_language}:\n")
+
+chain = prompt | llm
+response = chain.invoke(
+    {
+        "output_language": "German",
+        "input": "I love programming.",
+    }
+)
+
+print(response)
